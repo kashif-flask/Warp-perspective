396bedb9cb41bd369e2ef7c3443981f2b6ee095f
kashif-flask <70099921+kashif-flask@users.noreply.github.com>
2021-10-09 14:56:34 +0530

    Update warp.py

--- warp.py
+++ warp.py
@@ -2,15 +2,13 @@ import cv2
 import numpy as np
 
 
-#
-
 def processing(img):
     imggray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
     #cv2.imshow("imagegray",imggray)
-    imghsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    #imghsv=cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
     #cv2.imshow("imagehsv",imghsv)
-    imgcanny=cv2.Canny(imggray,200,200)
-    cv2.imshow("imagecanny",imgcanny)
+    #imgcanny=cv2.Canny(imggray,200,200)
+    #cv2.imshow("imagecanny",imgcanny)
     imgblur=cv2.GaussianBlur(imggray,(5,5),1)
     #cv2.imshow("imageblur",imgblur)
     imgcannyblur=cv2.Canny(imgblur,200,200)
@@ -19,7 +17,7 @@ def processing(img):
     imgdilate=cv2.dilate(imgcannyblur,kernel,iterations=2)
     #cv2.imshow("imgdilate",imgdilate)
     imgerode=cv2.erode(imgdilate,kernel,iterations=1)
-    #cv2.imshow("imageerode",imgerode)
+   # cv2.imshow("imageerode",imgerode)
     return imgerode
 
 def getcontour(imgerode,img2):
@@ -61,26 +59,38 @@ def reorder(points):
 #print(newpoints)
 #img=cv2.imread("card1.png")
 #img=cv2.resize(img,(0,0),None,0.4,0.4)
-cap=cv2.VideoCapture("vid1.mp4") //you can use webcam also
+cap=cv2.VideoCapture("vid1.mp4") #you can use webcam also
 
 while True:
     success,img=cap.read()
-    img=cv2.resize(img,(0,0),None,0.4,0.4)
-    img2=img.copy()
-    cv2.imshow("img",img)
-    size=img.shape
-    imgerode=processing(img)
-    biggest=getcontour(imgerode,img2)
-    cv2.imshow("contour",img2)
-    if biggest.size>0:
-        newpoints=reorder(biggest)
-        newpoints=np.float32(newpoints)
-        pts2=[[0,0],[size[1],0],[0,size[0]],[size[1],size[0]]]
-        pts2=np.float32(pts2)
-        matrix=cv2.getPerspectiveTransform(newpoints,pts2)
-        imgwrap=cv2.warpPerspective(img,matrix,(size[1],size[0]))
-        cv2.imshow("wrap",imgwrap)
-        
+    if success:
+        img=cv2.resize(img,(0,0),None,0.4,0.4)
+        img2=img.copy()
+        #cv2.imshow("img",img)
+        size=img.shape
+        imgerode=processing(img)
+        biggest=getcontour(imgerode,img2)
+        #cv2.imshow("contour",img2)
+        if biggest.size>0:
+            newpoints=reorder(biggest)
+            newpoints=np.float32(newpoints)
+            pts2=[[0,0],[size[1],0],[0,size[0]],[size[1],size[0]]]
+            pts2=np.float32(pts2)
+            matrix=cv2.getPerspectiveTransform(newpoints,pts2)
+            imgwarp=cv2.warpPerspective(img,matrix,(size[1],size[0]))
+            imgwarp_rotated=cv2.rotate(imgwarp,cv2.cv2.ROTATE_90_CLOCKWISE)
+            gray_warp=cv2.cvtColor(imgwarp_rotated,cv2.COLOR_BGR2GRAY)
+            mask=cv2.adaptiveThreshold(gray_warp,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,7,2)
+            #mask_blur=cv2.medianBlur(mask,5)
+            cv2.imshow("imgwarp",imgwarp_rotated)
+            cv2.imshow("mask",mask)
+            
+            
 
-    if cv2.waitKey(1) & 0xFF==ord('q'):
+        if cv2.waitKey(1) & 0xFF==ord('q'):
+            break
+    else:
         break
+    
+cap.release()
+cv2.destroyAllWindows
